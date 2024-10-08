4c4215810f09896054c583e379cbd6aea3681f0d
prabhjot-2 <prabh1146@gmail.com>
2024-10-08 23:52:53 +0530

    all pages sample created and first login page done userslice done for login page and login page working

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import ManageSkills from "./pages/ManageSkills";
+import ManageProjects from "./pages/ManageProjects";
+import UpdateProject from "./pages/UpdateProject";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import ManageTimeline from "./pages/ManageTimeline";
+import ViewProject from "./pages/ViewProject";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+        <Route path="/manage/skills" element={<ManageSkills />} />
+        <Route path="/manage/timeline" element={<ManageTimeline />} />
+        <Route path="/manage/projects" element={<ManageProjects />} />
+        <Route path="/view/project/:id" element={<ViewProject />} />
+        <Route path="/update/project/:id" element={<UpdateProject />} />
+      </Routes>
+      <ToastContainer position="bottom-right" theme="dark" />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ dashboard/src/pages/ManageProject.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManageProject = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ManageProject
