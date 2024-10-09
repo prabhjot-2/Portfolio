694bdca4891705027ac2bbb7f00b04d1348a0b50
prabhjot-2 <prabh1146@gmail.com>
2024-10-09 23:24:07 +0530

    forgot and reset password page created and working for both slice is write and

--- dashboard/src/store/store.jsx
+++ dashboard/src/store/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import forgotResetPassReducer from "./slices/forgotResetPasswordSlice";
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    forgotPassword:forgotResetPassReducer
   },
 });
