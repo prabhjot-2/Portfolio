7188fb02dcd841f695a2d385f3964c9a6ab668c1
prabhjot-2 <prabh1146@gmail.com>
2024-10-25 00:35:09 +0530

    registeration of user working again

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -11,8 +11,6 @@ export const register = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Avatar Required!", 400));
   }
   const { avatar, resume } = req.files;
-  console.log(`avatar`,avatar);
-  console.log(`resyme:`,resume);
   
   //POSTING AVATAR
   const cloudinaryResponseForAvatar = await cloudinary.uploader.upload(
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,27 @@
-export const generateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonWebToken();
-    res
-      .status(statusCode)
-      .cookie("token", token, {
-        expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-      })
-      .json({
-        success: true,
-        message,
-        user,
-        token,
-      });
+export const generateToken = (user, message, statusCode, res) => 
+  {
+    try {
+      // Generate the JWT token
+      const token = user.generateJsonWebToken();
+      
+      // Get cookie expiration value (fallback to 7 days if not set)
+      const cookieExpireDays = process.env.COOKIE_EXPIRE || 7;
+  
+      // Set token in HTTP cookie
+      res
+        .status(statusCode)
+        .cookie("token", token, {
+          expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
+          httpOnly: true, // Cookie cannot be accessed via JavaScript
+        })
+        .json({
+          success: true,
+          message,
+          user, // Be careful not to send sensitive user data
+          token,
+        });
+    } catch (error) {
+      // If JWT generation fails, send an error response
+      res.status(500).json({ success: false, message: "Token generation failed." });
+    }
   };
