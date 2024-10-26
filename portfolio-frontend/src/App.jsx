51c0490ccb4a7bb70e9f252fc5fa994fe8245442
prabhjot-2 <prabh1146@gmail.com>
2024-10-27 00:25:03 +0530

    portfolio-frontend started

---
+++ portfolio-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ThemeProvider } from './components/theme-provider'
+
+function App() {
+  
+
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
