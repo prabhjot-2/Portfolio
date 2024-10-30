b04e7765d4bcda1239ca8ebb946d0eb3aaa29ba2
prabhjot-2 <prabh1146@gmail.com>
2024-10-30 23:48:07 +0530

    contact, about, myApps and skills section done

--- portfolio-frontend/src/pages/miniComponents/About.jsx
+++ portfolio-frontend/src/pages/miniComponents/About.jsx
@@ -32,8 +32,7 @@ const About = () => {
           </div>
           <div className="flex justify-center flex-col tracking-[1px] text-xl gap-5">
             <p>
-              My name is Zeeshan, but my followers call me Zeeshu. I will
-              graduate in Software Engineering from SMIU around 2024. I work as
+              more to add on this later I work as
               a web developer and freelancer. My hobbies include watching
               movies, series, playing video games, and occasionally cooking.
             </p>
